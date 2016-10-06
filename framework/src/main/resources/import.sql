ddea307335ae196acb38607aa586efe83abf207f
m-cakir <mcakir.web@gmail.com>
2016-10-06 12:33:25 +0300

    - import.sql added

---
+++ framework/src/main/resources/import.sql
@@ -0,0 +1,3 @@
+insert into role (name) values ('ADMIN')
+
+insert into user (username, password, role_id, enabled, login_failures_count) values ('bootiful@spring.com', '$2a$06$q85WkteQWnXKNrfjTibl5uKLoHxny6KKzHBPcmhM8ss8UoU1FTmwu', 1, 1, 0)
